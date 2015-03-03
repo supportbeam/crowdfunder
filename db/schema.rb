3eb387dc62542b2577e5d135895563a58f29a1cf
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-02 20:56:16 -0500

    Change Database From SQLite To Postgres
    Change the database from SQLite to Postgres because Postgres is what is
    used on most Rails Application then SQLite which should not be on a live
    Rails Application.

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,38 @@
 
 ActiveRecord::Schema.define(version: 20150302225935) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaigns", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "funding_goal"
     t.date     "start_date"
     t.date     "end_date"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "time"
+    t.integer  "restaurant_id"
+    t.integer  "user_id"
+    t.integer  "party_size"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "reservations", ["restaurant_id"], name: "index_reservations_on_restaurant_id", using: :btree
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.text     "address"
+    t.text     "description"
+    t.integer  "capacity"
+    t.time     "hours_open"
+    t.time     "hours_closed"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
@@ -31,6 +57,18 @@ ActiveRecord::Schema.define(version: 20150302225935) do
     t.datetime "updated_at",    null: false
   end
 
-  add_index "rewards", ["campaign_id"], name: "index_rewards_on_campaign_id"
+  add_index "rewards", ["campaign_id"], name: "index_rewards_on_campaign_id", using: :btree
 
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_foreign_key "reservations", "restaurants"
+  add_foreign_key "reservations", "users"
+  add_foreign_key "rewards", "campaigns"
 end
