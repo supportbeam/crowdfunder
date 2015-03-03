decb2b2255f68e1328b0bfd620a40bb399655ca3
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-03 13:24:51 -0500

    Fix Foreign Tables In Database
    Because the rake db:setup will automatically add in tables from
    database with the same name. The name should be different in order to
    avoid having foreign tables from another project in this project.

--- db/schema.rb
+++ db/schema.rb
@@ -26,29 +26,6 @@ ActiveRecord::Schema.define(version: 20150302225935) do
     t.datetime "updated_at",   null: false
   end
 
-  create_table "reservations", force: :cascade do |t|
-    t.datetime "time"
-    t.integer  "restaurant_id"
-    t.integer  "user_id"
-    t.integer  "party_size"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "reservations", ["restaurant_id"], name: "index_reservations_on_restaurant_id", using: :btree
-  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string   "name"
-    t.text     "address"
-    t.text     "description"
-    t.integer  "capacity"
-    t.time     "hours_open"
-    t.time     "hours_closed"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
   create_table "rewards", force: :cascade do |t|
     t.text     "description"
     t.integer  "pledge_amount"
@@ -59,16 +36,5 @@ ActiveRecord::Schema.define(version: 20150302225935) do
 
   add_index "rewards", ["campaign_id"], name: "index_rewards_on_campaign_id", using: :btree
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_foreign_key "reservations", "restaurants"
-  add_foreign_key "reservations", "users"
   add_foreign_key "rewards", "campaigns"
 end
