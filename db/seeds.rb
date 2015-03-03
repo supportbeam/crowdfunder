65ecd5c9a139efc51bf1632f040bd2bd552926b3
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-03 16:50:57 -0500

    Add Database Seeds For Testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Campaign.destroy_all
+
+100.times do
+	Campaign.create({
+		title: Faker::Company.name,
+		description: Faker::Company.bs << " " << Faker::Company.catch_phrase,
+		funding_goal: Faker::Number.number(6),
+		start_date: Faker::Date.between(Date.today, 25.days.from_now), 
+		end_date: Faker::Date.between(25.days.from_now, 50.days.from_now)
+	})
+end
+
+4.times do
+	User.create!({
+		email: Faker::Internet.safe_email(),
+		first_name: Faker::Name.first_name,
+		last_name: Faker::Name.last_name,
+		password: "password",
+		password_confirmation: "password"
+	})
+end
