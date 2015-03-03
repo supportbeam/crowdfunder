36565ea48530bec29bbb99d4ec5e3c46f6f4b052
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-03 14:54:48 -0500

    Add Routes To Sessions To Login & Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :user_sessions, only: [:create]
   resources :users, only: [:create]
-  get 'signup' => 'users#new', :as => :signup
+  get  'signup' => 'users#new', :as => :signup
+  get  'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
