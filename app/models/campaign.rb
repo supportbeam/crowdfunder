8ace181a82ee2553b4ffaa0041b3f98a4f257970
JJLim <lim.jess9@gmail.com>
2015-03-04 18:31:44 -0500

    Add rspec helpers and campaign rspec

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,4 +2,9 @@ class Campaign < ActiveRecord::Base
   has_many :rewards
   has_many :pledges
   accepts_nested_attributes_for :rewards, :reject_if => :all_blank, :allow_destroy => true
+
+  def days_available
+    
+  end
+
 end
