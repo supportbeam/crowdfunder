ac24d956afe96b6c76a79bb07b10fc8623afa57f
JJLim <lim.jess9@gmail.com>
2015-03-04 18:31:44 -0500

    Add rspec helpers and campaign rspec

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :campaign do
-    
+    title {Faker::Name.name}
+    funding_amount {Faker::Number.number(5)}
+    start_date {Faker::Date.backward(4)}
+    end_date {Faker::Date.between(Date.today, 14.days.from_now)}
   end
 
 end
