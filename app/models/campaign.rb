0a206b717d4e5e244b3b10a8e3a8d57d9934ff22
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-05 16:32:10 -0500

    Add User As Campaign Owner To Know Who Owns It

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,6 @@
 class Campaign < ActiveRecord::Base
   has_many :rewards
   has_many :pledges
+	belongs_to :user
   accepts_nested_attributes_for :rewards, :reject_if => :all_blank, :allow_destroy => true
 end
