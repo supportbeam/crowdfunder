c3f9eeef229d8b4959b98aa10fa72d3f8ea1e397
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-06 16:38:45 -0500

    Change Start & End Date In Factory To Test

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :campaign do
     title {Faker::Name.name}
-    funding_goal {Faker::Number.number(5)}
-    start_date {Faker::Date.backward(4)}
-    end_date {Faker::Date.between(Date.today, 14.days.from_now)}
+    funding_goal Faker::Number.number(5)
+    start_date Time.now
+    end_date Faker::Date.between(Time.now.tomorrow, 14.days.from_now)
   end
-
 end
