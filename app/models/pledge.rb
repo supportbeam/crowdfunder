2e8a612b6d0cdecaa230847484d4cd220b4cb737
AStox <Adamstox@gmail.com>
2015-03-06 15:33:58 -0500

    added total amount pledged to profile page

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,4 +2,13 @@ class Pledge < ActiveRecord::Base
   belongs_to :user
 	belongs_to :campaign
   belongs_to :reward
+
+  def self.user_total(user)
+  	total = 0
+  	Pledge.where(user: user).each do |pledge|
+  		total += pledge.donation_amount
+  	end
+  	total
+  end
+
 end
