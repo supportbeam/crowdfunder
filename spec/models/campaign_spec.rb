859ca7f40701973a20816cb0a9f9315654f1a7ff
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-06 15:52:32 -0500

    Remove days_left Must Be Zero Test
    Remove the days_left must be zero test as the end date and todays date
    will always be changing making it hard to test.
    
    Also move the let method to be outside the description for days_left in
    order for other test to use it.

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,18 +1,11 @@
 require 'rails_helper'
 
 describe Campaign do
-  describe "#days left" do
-    let!(:campaign) {create(:campaign)}
+	let!(:campaign) {create(:campaign)}
 
-    it "should return 0 if end_date has passed" do
-      expect(campaign.days_available).to eq(0)
-    end
-
+	describe "#days_left" do
     it "should return not return a negative number" do
-      expect(campaign.days_available).to be > 0
+      expect(campaign.days_left).to be >= 0
     end
-
-    
-
   end
 end
