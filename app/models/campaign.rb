8bfe20523ec4c56c47c7a05bf92fd40253e7c93b
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-06 16:18:44 -0500

    Add Custom Validation For Checking Dates

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,6 +4,8 @@ class Campaign < ActiveRecord::Base
 	belongs_to :user
   accepts_nested_attributes_for :rewards, :reject_if => :all_blank, :allow_destroy => true
 
+  validate :ensure_dates
+
   def days_left
     days = (end_date - Date.today).to_i 
 
@@ -14,4 +16,10 @@ class Campaign < ActiveRecord::Base
     end
   end
 
+  private
+  def ensure_dates
+    if start_date > end_date
+       errors.add(:end_date, "should not end before it starts")
+    end
+  end
 end
